9a92ee381c4f21f664f2a2bbb0c3dd9980cdf129
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-09-22 18:30:26 +0300

    Add files via upload

---
+++ Lab01/src/main/java/com/lab01/forms/PersonForm.java
@@ -0,0 +1,14 @@
+package com.lab01.forms;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonForm {
+ private String name;
+ private int age;
+}
---
+++ Lab01/src/main/java/com/lab01/model/Person.java
@@ -0,0 +1,13 @@
+package com.lab01.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private String name;
+    private int age;
+}
