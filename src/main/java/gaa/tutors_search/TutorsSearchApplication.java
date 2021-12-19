ddaf06208c2f459d3005740eccc9c6e473276fab
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-12-19 22:32:00 +0300

    Add files via upload

---
+++ src/main/java/gaa/tutors_search/TutorsSearchApplication.java
@@ -0,0 +1,13 @@
+package gaa.tutors_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorsSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorsSearchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/gaa/tutors_search/repository/ContractForm.java
@@ -0,0 +1,6 @@
+package gaa.tutors_search.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractForm extends JpaRepository<ContractForm , Long> {
+}
