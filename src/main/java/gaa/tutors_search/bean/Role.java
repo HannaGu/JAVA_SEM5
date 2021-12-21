06bdd0115b6706aa26f6c5723889e98f24afbe6f
HannaGu <70803052+HannaGu@users.noreply.github.com>
2021-12-21 22:29:30 +0300

    Delete src/main/java/gaa/tutors_search directory

--- src/main/java/gaa/tutors_search/bean/Role.java
+++
@@ -1,7 +0,0 @@
-package gaa.tutors_search.bean;
-
-public enum Role {
-    ROLE_ADMIN,
-    ROLE_USER,
-    ROLE_TUTOR
-}
--- src/main/java/gaa/tutors_search/repository/ContractForm.java
+++
@@ -1,6 +0,0 @@
-package gaa.tutors_search.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContractForm extends JpaRepository<ContractForm , Long> {
-}
